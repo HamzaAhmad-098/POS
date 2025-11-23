8ec66a7d21bd6003a8423fb8fe72c04cf1549558
Hamza Ahmad <jhaji1223@gmail.com>
2025-11-23 11:40:29 +0500

    Products issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@
 class DashboardController < ApplicationController
   def show
     if Current.shop
-      @products = Product.all
+      @products = Current.shop.products
       @today_sales = Current.shop.sales.where(created_at: Time.current.all_day)
       @total_sales = @today_sales.sum(:total_cents).to_f / 100.0
       @low_stock_count = Current.shop.products.where("current_stock <= reorder_level").count
@@ -11,5 +11,4 @@ class DashboardController < ApplicationController
       redirect_to login_path, alert: "No shop associated with your account."
     end
   end
-  
 end
