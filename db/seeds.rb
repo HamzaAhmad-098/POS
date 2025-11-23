87ca9fdedb8b667065dd37bb72156f2a14a0afce
Hamza Ahmad <jhaji1223@gmail.com>
2025-11-23 11:28:51 +0500

    Migration handle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,22 @@
-# 1. Clean the database (Optional but recommended)
-# This prevents "Email has already been taken" errors if you run seeds twice.
-puts "🗑️  Cleaning up old data..."
-User.destroy_all
-Shop.destroy_all
 
-puts "🌱 Seeding data..."
+
 
 # 2. Create the Shop
 puts "  Creating Shop..."
 shop = Shop.create!(
-  name: "Test Shop",
-  address: "Test Address",
-  phone: "03001234567"
+  name: "M.Beauty Bloom",
+  address: "Urdu Bazaar Beside Nazir cloth House",
+  phone: "03434801621"
 )
 puts "  ✅ Shop created: '#{shop.name}' (ID: #{shop.id})"
 
 # 3. Create the User associated with the Shop
 puts "  Creating Owner..."
 user = User.create!(
-  full_name: "Test Owner",
-  email: "test@example.com",
-  phone: "03001234567",
-  password: "password123",
+  full_name: "Muddasir",
+  email: "muddasir@hamzaxdevelopers.com",
+  phone: "03434801621",
+  password: "muddasir1234",
   role: "owner",
   shop: shop # Rails automatically handles the foreign key (shop_id) here
 )
